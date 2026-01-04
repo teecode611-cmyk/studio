85061306158be5a4a7e2309b2f2c539da94ed6a8
Tori <teecode611@gmail.com>
2026-01-04 01:00:24 +0000

    the error messaging is still red, make it gold

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -157,7 +157,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-accent-foreground", className)}
+      className={cn("text-sm font-medium text-destructive", className)}
       {...props}
     >
       {body}
